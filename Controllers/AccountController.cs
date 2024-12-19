3e086bfab0cb3065ed7692deae7c64a7c883ea08
Sudi Hardi <owner@sudihardi.eu.org>
2024-12-19 22:24:32 +0100

    initial

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Models;
+
+namespace ShoppingCart.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // Display the registration form
+        public IActionResult Create() => View();
+
+        // Handle the registration logic
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new AppUser
+                {
+                    UserName = user.UserName,
+                    Email = user.Email,
+                };
+
+                // Create the user in the database
+                var result = await _userManager.CreateAsync(newUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    // If successful, sign the user in and redirect to the dashboard
+                    await _signInManager.SignInAsync(newUser, isPersistent: false);
+                    return Redirect("/admin/products"); // Adjust this redirect as needed
+                }
+
+                // If creation fails, add errors to the ModelState
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCart.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password), Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; } // Add this property to handle confirm password
+    }
+}
